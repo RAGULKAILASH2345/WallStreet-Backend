dff2482e2c749a6b5fa0b6aa0e999b171b2c0cc5
Ragul Kailash M <ragulkailash12@gmail.com>
2025-02-09 12:25:44 +0530

    Updated config.js file

--- config/config.js
+++ config/config.js
@@ -1,30 +1,46 @@
+require("dotenv").config();
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "{123456}",
-    "database": "wallstreet",
-    "host": "127.0.0.1",
-    "port": "3306",
-    "dialect": "mysql",
-    "define": {
-      "freezeTableName": true
-    }
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "use_env_variable": "DB_URL",
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": true
+  development: {
+    use_env_variable: "DB_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-    "define": {
-      "freezeTableName": true // see https://sequelize.org/docs/v6/core-concepts/model-basics/#enforcing-the-table-name-to-be-equal-to-the-model-name
-    }
-  }
-}
+    define: {
+      freezeTableName: true,
+    },
+    logging: false, 
+  },
+  test: {
+    use_env_variable: "DB_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      freezeTableName: true,
+    },
+    logging: false,
+  },
+  production: {
+    use_env_variable: "DB_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      freezeTableName: true,
+    },
+    logging: false,
+  },
+};
